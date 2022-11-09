e97fb63f1d1c8337a9f5b6c682972a376f8f530d
vboron <ucbtvbo@ucl.ac.uk>
2022-11-09 00:48:33 +0000

    add print

--- runAGL.py
+++ runAGL.py
@@ -18,6 +18,7 @@ import pandas as pd
 
 def extract_data(dire):
     for file in os.listdir(dire):
+        print(file)
         if file.endswith('.pdb'):
             with open(file, "r") as f:
                 for line in f:
