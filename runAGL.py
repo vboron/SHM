206cc514065effa93f5e8d69854b73521cfe9f4c
vboron <ucbtvbo@ucl.ac.uk>
2022-11-23 01:35:38 +0000

    split on \n and :

--- runAGL.py
+++ runAGL.py
@@ -34,7 +34,7 @@ def extract_data(dire):
             result = (subprocess.check_output(['agl', '-a', path])).decode("utf-8")
             result = result.replace(' ', '')
             print(result)
-            result_data = re.split('\n| ', result)
+            result_data = re.split('\n|:', result)
             # result_data = result.split('\n', ' ')
             print(result_data)
             # for data in result_data:
