8fc70d9de6036c1ff14ae633ed46149a169119de
vboron <ucbtvbo@ucl.ac.uk>
2022-12-06 15:27:34 +0000

    Remove print

--- runAGL.py
+++ runAGL.py
@@ -41,7 +41,6 @@ def parse_redund_file(red_file):
         for line in lines:
             filter_line(line)
             print(line)
-            print(lines)
             lines.remove(line)
         print(len(lines))
 
