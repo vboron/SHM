d1130ac510dc00dd7d9f074189963f8f8168795b
vboron <ucbtvbo@ucl.ac.uk>
2023-02-07 17:35:13 +0000

    add value as a list

--- filter_pdb_byspecies.py
+++ filter_pdb_byspecies.py
@@ -27,7 +27,10 @@ def find_lines(dire):
                         info = line_s[1].replace(';', '')
                         rel_lines.append(info.strip())
         for x, y in pairwise(rel_lines):
-            data[x] = y
+            if x in data:
+               data[x].append(y) 
+            else:
+                data[x] = [y]
         print(data)
 
 
