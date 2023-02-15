f9b43bc8f4afec45db86b56b41e125c9c773fde0
vboron <ucbtvbo@ucl.ac.uk>
2023-02-15 16:05:58 +0000

    only add H or L chains

--- filter_pdb_byspecies.py
+++ filter_pdb_byspecies.py
@@ -31,10 +31,11 @@ def map_molid_chain(dire, files):
                         rel_lines.append(info.strip())
         # print(rel_lines)
         for x, y in pairwise(rel_lines):
-            if x in chain_data:
-               chain_data[x].append(y) 
-            else:
-                chain_data[x] = [y]
+            if 'H' in y or 'L' in y:
+                if x in chain_data:
+                    chain_data[x].append(y) 
+                else:
+                    chain_data[x] = [y]
         print(chain_data)
         # for value in chain_data.values():
         #     print(file, value)
@@ -46,41 +47,7 @@ def map_molid_chain(dire, files):
         #         h_chain.append(value[1])
         # summary = summary + h_chain + l_chain
         # print(summary)
-    # line_terms = ['MOL_ID:', 'CHAIN:', 'ORGANISM_SCIENTIFIC:']
-    # species_info = []
-    # for file in files:
-    #     chains = []
-    #     orgs = []
-    #     l_chain = []
-    #     h_chain = []
-    #     summary = [file[:-4]]
-    #     data = {}
-    #     rel_lines = []
-    #     with open(os.path.join(dire, file), 'r') as f:
-    #         for line in f:
-    #             if 'MOL_ID:' in line:
-    #                 if 
-    #             # for term in line_terms:
-    #             #     if term in line:
-    #             #         line_s = line.split(':')
-    #             #         info = line_s[1].replace(';', '')
-    #             #         rel_lines.append(info.strip())
-    #     for x, y in pairwise(rel_lines):
-    #         if x in data:
-    #            data[x].append(y) 
-    #         else:
-    #             data[x] = [y]
-    #     for value in data.values():
-    #         print(file, value)
-    #         if 'L' in value[0]:
-    #             # l_chain.append('L')
-    #             l_chain.append(value[1])
-    #         if 'H' in value[0]:
-    #             # h_chain.append('H')
-    #             h_chain.append(value[1])
-    #     summary = summary + h_chain + l_chain
-    #     species_info.append(summary)
-    # return species_info
+ 
 
 
 def make_df(data):
