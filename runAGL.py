6d4fca340116c127a5f605553f2c8fd9af45044e
vboron <ucbtvbo@ucl.ac.uk>
2023-03-06 16:03:41 +0000

    Print column names

--- runAGL.py
+++ runAGL.py
@@ -160,7 +160,7 @@ def extract_data(fastadir, pdbdir, files, dictionary):
 def find_maxrange_per_mutation_count(df):
     max_df = df.groupby(by = ['total_mut'])['angle_range'].max()
     max_df.columns = ['total_mut', 'angle_range']
-    print(max_df)
+    print(max_df.columns)
 
 
 def make_graphs(df, graph_name):
