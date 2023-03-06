46cc6d4ff49643d0a95eb2ce5425ab24e2179e30
vboron <ucbtvbo@ucl.ac.uk>
2023-03-06 17:29:44 +0000

    Reset index in place

--- runAGL.py
+++ runAGL.py
@@ -159,9 +159,12 @@ def extract_data(fastadir, pdbdir, files, dictionary):
 
 def find_maxrange_per_mutation_count(df):
     max_df = df.groupby('total_mut').angle_range.agg(['max'])
-    max_df.reset_index()
-    max_df.columns = ['total_mut', 'angle_range']
     print(max_df)
+    max_df.reset_index(inplace = True)
+    # max_df.columns = ['total_mut', 'angle_range']
+    print(max_df)
+    # max_df.reset_index()
+    # print(max_df)
     print(max_df.columns)
 
 
