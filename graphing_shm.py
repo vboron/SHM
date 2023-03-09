e6cac25869f1df2adbee07221b350f842c1861e3
vboron <ucbtvbo@ucl.ac.uk>
2023-03-09 14:33:33 +0000

    Make graph scales smaller

--- graphing_shm.py
+++ graphing_shm.py
@@ -26,8 +26,8 @@ def mutations_vs_angrange(df, mut_column, x_axis, directory, name, max_val_df):
 
     # Sets the maximum and minimum values for the axes
     # axes.autoscale(tight=True)
-    axes.set_xlim([0, 60])
-    axes.set_ylim([-5, 45])
+    axes.set_xlim([0, 50])
+    axes.set_ylim([-5, 20])
 
     # axes.axline((0, 0), (1, 1), color='k')
 
@@ -36,7 +36,7 @@ def mutations_vs_angrange(df, mut_column, x_axis, directory, name, max_val_df):
     plt.ylabel('Range of the packing angle')
 
     # Adds graph annotations
-    plt.text(s=f'Correlation: {pearson_a}', x=5, y=40, fontsize=8)
+    plt.text(s=f'Correlation: {pearson_a}', x=10, y=17, fontsize=8)
 
     # plt.tight_layout()
     # Plot highest values
@@ -51,7 +51,7 @@ def mutations_vs_angrange(df, mut_column, x_axis, directory, name, max_val_df):
              linestyle='dashed', linewidth=1)
     bf_line = 'y={:.3f}x+{:.3f}'.format(m, b)
     plt.text(s=f'Best fit: {bf_line}',
-             x=5, y=38, fontsize=8, color=color_bf_line)
+             x=10, y=15, fontsize=8, color=color_bf_line)
 
     # Exports the figure as a .jpg file
     path_fig = os.path.join(directory, f'{name}.jpg')
