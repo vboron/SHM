eb9b05af24a6885e974dfef1a05afa4bbd2e70f5
vboron <ucbtvbo@ucl.ac.uk>
2023-03-23 15:04:55 +0000

    Add graphing for hydrophobicity values

--- graphing_shm.py
+++ graphing_shm.py
@@ -41,7 +41,6 @@ def mutations_vs_angrange(df, mut_column, x_axis, directory, name, max_val_df):
     # Adds graph annotations
     plt.text(s=f'Correlation: {pearson_a}', x=10, y=17, fontsize=8)
 
-    # plt.tight_layout()
     # Plot highest values
     def plotting(x, y, color):
         plt.scatter(x, y, s=3, color=color)
@@ -54,15 +53,53 @@ def mutations_vs_angrange(df, mut_column, x_axis, directory, name, max_val_df):
     
     plotting(x_all, y_all, color_all)
     plotting(x_max, y_max, color_top)
-    # plt.scatter(x, y, s=3, color=color_values)
-    # plt.scatter(x_max, y_max, s=3, color=color_bf_line)
 
-    # m, b = np.polyfit(x_max, y_max, 1)
-    # plt.plot(x, m * x + b, color=color_bf_line,
-    #          linestyle='dashed', linewidth=1)
-    # bf_line = 'y={:.3f}x+{:.3f}'.format(m, b)
-    # plt.text(s=f'Best fit: {bf_line}',
-    #          x=10, y=15, fontsize=8, color=color_bf_line)
+    # Exports the figure as a .jpg file
+    path_fig = os.path.join(directory, f'{name}.jpg')
+    plt.savefig(path_fig, format='jpg')
+    plt.close()
+
+    # return m, b
+    return
+
+
+def hydrophobicity_vs_mutations(x_values, y_values, directory, name):
+    # Plot all data
+    # .corr() returns the correlation between two columns
+    pearson_a = x_values.corr(y_values)
+
+    plt.figure()
+
+    color_data = 'burlywood'
+    color_bf = 'rebeccapurple'
+
+    axes = plt.gca()
+
+    # Sets the maximum and minimum values for the axes
+    # axes.autoscale(tight=True)
+    axes.set_xlim([0, 50])
+    axes.set_ylim([-0.5, 10])
+
+    # axes.axline((0, 0), (1, 1), color='k')
+
+    # Sets the axes labels
+    plt.xlabel(f'Mutations from germline (VH&VL)')
+    plt.ylabel('Change in hydrophobicity')
+
+    # Adds graph annotations
+    plt.text(s=f'Correlation: {pearson_a}', x=10, y=9, fontsize=8)
+
+    # Plot highest values
+    def plotting(x, y, color):
+        plt.scatter(x, y, s=3, color=color_data)
+        m, b = np.polyfit(x, y, 1)
+        plt.plot(x, m * x + b, color=color_bf,
+             linestyle='dashed', linewidth=1)
+        bf_line = 'y={:.3f}x+{:.3f}'.format(m, b)
+        plt.text(s=f'Best fit: {bf_line}',
+             x=10, y=15, fontsize=8, color=color)
+    
+    plotting(x_values, y_values)
 
     # Exports the figure as a .jpg file
     path_fig = os.path.join(directory, f'{name}.jpg')
