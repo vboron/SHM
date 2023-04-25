bdff6f7a35ae9dc036757bf09f77752de244d456
vboron <ucbtvbo@ucl.ac.uk>
2023-04-25 13:33:51 +0100

    print counts for specific labels

--- hydrophob_changes.py
+++ hydrophob_changes.py
@@ -118,7 +118,8 @@ def extract_mut_data(fastadir):
         df['input_class'] = df['input'].map(hydrophobicity_class)
         df['germ_class'] = df['germline'].map(hydrophobicity_class)
         df_clear = df[df['input_class'] != df['germ_class']]
-        print(df_clear['input_class'].value_counts())
+        print(df_clear['input_class'].value_counts()['hydrophilic'])
+        print(df_clear['input_class'].value_counts()['hydrophobic'])
         # df['input_hydrophob'] = df['input'].map(
         #     lambda x: utils_shm.hydrophobicity(x))
         # df['germ_hydrophob'] = df['germline'].map(
