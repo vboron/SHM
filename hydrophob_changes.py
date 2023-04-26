245203a82851cfff5048d00fd26322b672eee6c6
vboron <ucbtvbo@ucl.ac.uk>
2023-04-26 16:40:32 +0100

    sort df

--- hydrophob_changes.py
+++ hydrophob_changes.py
@@ -218,6 +218,7 @@ def extract_mut_data(fastadir):
                       'hydrophilics_H1', 'hydrophobics_H1', 'hydrophilics_H2', 'hydrophobics_H2'])
     # df_hydroph = df_hydroph.astype({'dH_all': 'float64', 'dH_L1': 'float64', 'dH_L2': 'float64', 'dH_L3': 'float64', 
     #                                 'dH_H1': 'float64', 'dH_H2': 'float64'})
+    df_hydroph.sort_values('mut_count', inplace=True)
     df_hydroph.to_csv('introduced_hydrophobicity.csv', index=False)
     # graph.hydrophobicity_histagram([df_hydroph['dH_all'], df_hydroph['dH_L1'], df_hydroph['dH_L2'], 
     #                                    df_hydroph['dH_L3'], df_hydroph['dH_H1'], df_hydroph['dH_H2']], 
