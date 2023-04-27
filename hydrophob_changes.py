2d2d874016b6a109a32e1c319de37cb041406684
vboron <ucbtvbo@ucl.ac.uk>
2023-04-27 14:44:26 +0100

    call graph func on df not list (oops)

--- hydrophob_changes.py
+++ hydrophob_changes.py
@@ -223,8 +223,8 @@ def extract_mut_data(fastadir):
     df_dist = df_hydroph[['code','mut_count', 'hydrophilics_all', 'hydrophobics_all']]
     df_dist['fraction_hydrophilic'] = df_dist['hydrophilics_all'] / df_dist['mut_count']
     df_dist['fraction_hydrophobic'] = df_dist['hydrophobics_all'] / df_dist['mut_count']
-    df_dist = df_dist.astype({'mut_count': 'int64', 'fraction_hydrophilic': 'float64', 'fraction_hydrophobic': 'float64'})
-    graph.introduced_fractional_hydrophobicity([df_dist['fraction_hydrophobic']], 'foo')
+    # df_dist = df_dist.astype({'mut_count': 'int64', 'fraction_hydrophilic': 'float64', 'fraction_hydrophobic': 'float64'})
+    graph.introduced_fractional_hydrophobicity(df_dist, 'foo')
     # df_hydroph.to_csv('introduced_hydrophobicity.csv', index=False)
     # graph.hydrophobicity_histagram([df_hydroph['dH_all'], df_hydroph['dH_L1'], df_hydroph['dH_L2'], 
     #                                    df_hydroph['dH_L3'], df_hydroph['dH_H1'], df_hydroph['dH_H2']], 
